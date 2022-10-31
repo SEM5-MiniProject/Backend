dacd6e6503f64b382b1d0a6cd7cb61e9974fcb86
iamkunal2k <kunalkumbhare8@gmail.com>
2022-10-31 21:46:08 +0530

    added controller for updated and delete items

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,5 +1,9 @@
 const Cart = require('../model/cart');
-const log  = require('../log')
+const log  = require('../log');
+const async = require('hbs/lib/async');
+const food = require('../model/food');
+const cart = require('../model/cart');
+
 const add_to_cart = async(req, res)=> {
     try {
         
@@ -18,6 +22,58 @@ const add_to_cart = async(req, res)=> {
       }
 }
 
-module.exports = {
-    add_to_cart 
+const deleteitems =  async (req, res) => {
+    try {
+        
+        // const cart_obj = new Cart ({
+        //     food_id:req.body.food,
+        //     quantity:req.body.quantity,
+        //     price:req.body.price
+        // });
+
+        // const cartData = cart_obj.save()
+        // res.status(200).send({success:true, msg:"Product details", data:cartData})
+
+        // const isPresent = Cart(quantity)
+        // if (isPresent >= 0) {
+        //     delete cart.food.splice(isPresent, 0);
+        //     cart.totalPrice -= food.price * Cart.quantity;
+        // }
+        const cartFind = await Cart.find({
+            $and:[
+                {user_id:req.user},
+                {food_id:req.body.food_id}
+            ]
+        })
+
+        await Cart.findByIdAndDelete (cartFind._id)
+        res.send("food Item deleted")
+        
+
+
+    } catch (err) {
+        log.error(err);
+        res.status(500).json({ error: 'Error in deleting the items' });
+    }
+}
+
+
+const updateCart = async (req, res) => {
+
+    const cartFind = await Cart.find({
+        $and:[
+            {user_id:req.user},
+            {food_id:req.body.food_id}
+        ]
+    })
+
+    const updateCart = await Cart.findByIdAndUpdate(
+        cartFind._id, {quantity:req.body.quantity}
+    )
+
+    res.send('quantity updated')
+}
+ 
+modules.exports = {
+    updateCart, deleteitems, add_to_cart
 }
