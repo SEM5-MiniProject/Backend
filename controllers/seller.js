91add975daf90d498b798a2d86ed8b5b48dcf93c
SalmanAd01 <salmanadhikari415@gmail.com>
2022-11-01 14:01:42 +0530

    Connect the add product with backend

--- controllers/seller.js
+++ controllers/seller.js
@@ -1,5 +1,7 @@
+const { storage } = require("../config/firebase");
 const log = require("../log");
 const Food = require("../model/food");
+const uuidv4 = require('uuid/v4');
 
 const getDashbord = (req, res) => {
   res.render('dashbord', {
@@ -23,8 +25,36 @@ const getAddProduct = (req, res) => {
     persist: req.persist,
   });
 };
+const postAddProduct = async(req, res) => {
+  try {
+    const bucket = storage.bucket();
+    const file = await bucket.upload(req.file.path, {
+      destination: uuidv4() + req.file.originalname,
+      metadata: {
+        contentType: req.file.mimetype,
+        metadata: {
+          firebaseStorageDownloadTokens: uuidv4(),
+        },
+      },
+    })
+    const url = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(file[0].name)}?alt=media&token=${file[0].metadata.metadata.firebaseStorageDownloadTokens}`;
+    const food = new Food({
+      ...req.body,
+      image: url,
+      belongsTo: req.seller._id,
+    });
+    await food.save();
+    req.flash('success', 'Product added successfully');
+    res.redirect('/dashboard');
+  } catch (err) {
+    log.error(err);
+    res.status(500).json({error: err.message});
+  }
+};
+
 module.exports = {
   getDashbord,
   getMyProducts,
   getAddProduct,
+  postAddProduct,
 };
