24a37cc6eee4de55aa0fbc4e9cd4e9ffb5bf6488
SalmanAd01 <salmanadhikari415@gmail.com>
2022-11-01 12:23:00 +0530

    showing added product and created modal to update food

--- controllers/seller.js
+++ controllers/seller.js
@@ -1,12 +1,22 @@
+const log = require("../log");
+const Food = require("../model/food");
+
 const getDashbord = (req, res) => {
   res.render('dashbord', {
     persist: req.persist,
   });
 };
-const getMyProducts = (req, res) => {
-  res.render('viewproduct', {
-    persist: req.persist,
-  });
+const getMyProducts = async(req, res) => {
+  try {
+    const foods = await Food.find({belongsTo: req.seller._id});
+    res.render('viewproduct', {
+      persist: req.persist,
+      foods: foods,
+    });
+  } catch (err) {
+    log.error(err);
+    res.redirect('/dashboard');
+  }
 };
 const getAddProduct = (req, res) => {
   res.render('addproduct', {
